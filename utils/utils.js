c459535592781ffd6ec324d785ccdb16998b0f95
downsview <15090804865@163.com>
2017-08-29 17:29:02 +0800

    add user interaction and trigonometric function

---
+++ utils/utils.js
@@ -0,0 +1,111 @@
+/**
+ * Created by Administrator on 2017/8/29.
+ */
+
+//将utils定义为window对象下的一个属性，属性值为对象
+window.utils = {};
+
+//在utils对象上定义鼠标捕获坐标的方法
+window.utils.captureMouse = function (element) {
+    //定义一个名为mouse的对象
+    var mouse = {x: 0, y: 0};
+
+    //为元素绑定mousemove事件
+    element.addEventListener('mousemove', function (event) {
+        var x, y;
+
+        //获取鼠标位于当前屏幕的位置，并做兼容处理
+        if (event.pageX || event.pageY) {
+            x = event.pageX;
+            y = event.pageY;
+        } else {
+            x = event.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+            y = event.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+        }
+
+        //将当前的坐标值减去元素的偏移位置，即为鼠标位于当前canvas的位置
+        x -= element.offsetLeft;
+        y -= element.offsetTop;
+
+        mouse.x = x;
+        mouse.y = y;
+    }, false);
+
+    //返回mouse对象
+    return mouse;
+}
+
+//在utils对象上定义手指触摸捕获坐标的方法
+window.utils.captureTouch = function (element) {
+    //定义一个名为touch的对象
+    var touch = {
+        x: null,
+        y: null,
+        isPressed: false,
+        event: null
+    };
+    var body_scrollLeft = document.body.scrollLeft,
+        body_scrollTop = document.body.scrollTop,
+        element_scrollLeft = document.documentElement.scrollLeft,
+        element_scrollTop = document.documentElement.scrollTop,
+        offsetLeft = element.offsetLeft,
+        offsetTop = element.offsetTop;
+
+    //绑定touchstart事件
+    element.addEventListener('touchstart', function (event) {
+        touch.isPressed = true;
+        touch.event = event;
+    }, false);
+
+    //绑定touchend事件
+    element.addEventListener('touchend', function (event) {
+        touch.isPressed = false;
+        touch.x = null
+        touch.y = null
+        touch.event = event;
+    }, false);
+
+    //绑定touchmove事件
+    element.addEventListener('touchmove', function (event) {
+        var x, y, touch_event = event.touches[0];
+
+        if (touch_event.pageX || touch_event.pageY) {
+            x = touch_event.pageX;
+            y = touch_event.pageY;
+        } else {
+            x = touch_event.clientX + body_scrollLeft + element_scrollLeft;
+            y = touch_event.clientY + body_scrollTop + element_scrollTop;
+        }
+
+        //减去偏移量
+        x -= offsetLeft;
+        y -= offsetTop;
+
+        touch.x = x;
+        touch.y = y;
+        touch.event = event;
+    }, false);
+
+    //返回touch对象
+    return touch;
+
+}
+
+//处理动画函数兼容性
+if (!window.requestAnimationFrame) {
+    window.requestAnimationFrame = window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.msRequestAnimationFrame ||
+        window.oRequestAnimationFrame ||
+        function (callback) {
+            return window.setTimeout(callback, 1000 / 60);
+        }
+}
+
+if(!window.cancelAnimationFrame) {
+    window.cancelAnimationFrame = window.webkitCancelAnimationFrame ||
+            window.mozCancelAnimationFrame ||
+            window.msCancelAnimationFrame ||
+            window.oCancelAnimationFrame ||
+            window.clearTimeout
+}
